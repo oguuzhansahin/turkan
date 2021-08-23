d842b6dbf4305dcf4e9b2d57450dabecbaecf4d6
oguuzhansahin <oguuzhansahiin@gmail.com>
2021-08-23 22:59:47 +0300

    add flask files

---
+++ credentials.py
@@ -0,0 +1,9 @@
+ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
+ACCESS_TOKEN_SECRET = "YOUR_ACCESS_TOKEN_SECRET"
+
+#Consumer KEYS
+API_KEY = "YOUR_API_KEY"
+API_SECRET_KEY = "YOUR_API_KEY_SECRET"
+
+FOOLISH = "JavaScript is not available. We’ve detected that JavaScript is disabled in this browser. Please enable JavaScript or switch to a supported browser to continue using twitter.com. You can see a list of supported browsers in our Help Center. Help Center"
+MODEL_NAME = "savasy/bert-base-turkish-squad"
